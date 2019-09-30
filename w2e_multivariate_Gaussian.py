cd777ab45e09e0757948156b006bc10522a0c48b
Brian Lambert <brlambert7818@gmail.com>
2019-09-30 11:49:00 +0100

    Update w2e_multivariate_Gaussian.py

--- w2e_multivariate_Gaussian.py
+++ w2e_multivariate_Gaussian.py
@@ -3,9 +3,21 @@ import matplotlib.pyplot as plt
 
 N = int(1e4)
 D = 2
-
+A = np.array([[1, 0], [0.5, 0.5]])
 X = np.random.randn(N, D)
+Z = A.dot(X.T)
+Z = Z.T
+
 plt.clf()
-plt.plot(X[:, 1], X[:, 0], '.')
+plt.plot(Z[:, 1], Z[:, 0], '.')
 plt.axis('square')
 plt.show()
+
+# prove theoretical cov[x1, x2] where x1=x2 is roughly a square matrix of ones
+# x1 = np.random.randn(50)
+# cov = np.cov(x1, x1)
+
+# def transformation(a):
+#     x = np.random.randn(2)
+
+
